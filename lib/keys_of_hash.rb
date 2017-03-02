986b5848ac01f9364d23f929f8efc26c366bf625
Robert Weber <rob.weber87@gmail.com>
2017-03-02 18:42:09 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
-  def keys_of(arguments)
-    result = []
-    
-    result
+  def keys_of(*arguments)
+
+    map {|key, value| arguments.include?(value) ? key : nil }.compact
+
   end
 end
